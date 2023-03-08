1b299f11aff7a1b01825f69cb80ca9927eeb4741
choojaeyong <researcherdragon2002@gmail.com>
2023-03-08 17:40:01 +0900

    제발 되죠...

---
+++ java_1st/src/main/java/com/example/java_1st/Java1stApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java1stApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java1stApplication.class, args);
+    }
+
+}
